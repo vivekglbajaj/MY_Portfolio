422c78b259130e6b1eeb0d0ae002e3b82d734240
vivekyadav <vivekyadavitc17@gmail.com>
2025-08-30 15:59:14 +0530

    Updated portfolio with small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> PedroTech </span>{" "}
+            <span className="text-glow text-foreground"> Vivek Kumar </span>{" "}
             Portfolio
           </span>
         </a>
